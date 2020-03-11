5940119cf3313b97de5c02a7e8613394b2e53d6d
DunaevaSvetlana <solnste1993@mail.ru>
2020-03-12 00:27:35 +0400

    Initial commit

---
+++ src/Souz.java
@@ -0,0 +1,12 @@
+public class Souz {
+    public static void main(String[] args) {
+        final int pehota = 5;
+        final int orki = 2;
+
+        final int souz = pehota + orki;
+
+        System.out.println("pehota = " + pehota);
+        System.out.println("orki = " + orki);
+        System.out.println("souz = " + souz);
+    }
+}
